fd39f85e11b3d0000c5009275e6204212a243385
Shivansh Nalwaya <shivansh@thebetterindia.com>
2020-12-28 13:49:57 +0530

    Start with watchlist feature

--- src/components/movie-card.js
+++ src/components/movie-card.js
@@ -1,10 +1,28 @@
-import _ from "lodash";
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import LottieView from "lottie-react-native";
+import React, { useRef } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const MovieCard = ({ movie }) => {
+  const animationRef = useRef();
+
+  const addToWatchlist = () => {
+    console.log(movie.watchlisted);
+    if (movie.watchlisted) animationRef.current.reset();
+    else animationRef.current.play();
+    movie.watchlisted = !movie.watchlisted;
+  };
+
   return (
     <View style={styles.cardContainer}>
+      <TouchableOpacity style={styles.likeButton} onPress={addToWatchlist}>
+        <LottieView
+          style={{ width: 25, height: 25 }}
+          source={require("../assets/animation.json")}
+          ref={animationRef}
+          loop={false}
+          speed={movie.watchlisted ? -1 : 1}
+        />
+      </TouchableOpacity>
       <Image source={{ uri: movie.Poster }} style={styles.poster} />
       <View style={styles.detailContainer}>
         <Text style={styles.movieTitle}>{movie.Title}</Text>
@@ -21,6 +39,19 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: "center",
     elevation: 5,
+  },
+  likeButton: {
+    height: 30,
+    width: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    position: "absolute",
+    zIndex: 2,
+    top: 5,
+    right: 5,
+    borderRadius: 50,
+    elevation: 3,
   },
   poster: {
     height: 200,
